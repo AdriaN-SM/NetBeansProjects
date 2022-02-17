50826fc734201136449beaf429083a7b4b8e52f2
AdriaN-SM <adrianarandiga30@gmail.com>
2022-02-17 23:47:46 +0100

    Agregado arrays2

---
+++ ejercicio_arrays2_02/src/ejercicio_arrays2_02/Ejercicio_arrays2_02.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio_arrays2_02;
+
+/**
+ *
+ * @author adria
+ */
+public class Ejercicio_arrays2_02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int arr1[] = new int[10];
+        int arr2[] = new int[10];
+        int arr3[] = new int[10];
+        
+    }
+    
+}
