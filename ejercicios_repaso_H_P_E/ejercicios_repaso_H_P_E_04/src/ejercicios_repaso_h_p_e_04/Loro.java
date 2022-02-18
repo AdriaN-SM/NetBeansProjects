4cc32ddc30eb6dc21b77dfd0fb70981e2d533685
AdriaN-SM <adrianarandiga30@gmail.com>
2022-02-18 19:36:52 +0100

    Añadido constructor y métodos

--- ejercicios_repaso_H_P_E/ejercicios_repaso_H_P_E_04/src/ejercicios_repaso_h_p_e_04/Loro.java
+++ ejercicios_repaso_H_P_E/ejercicios_repaso_H_P_E_04/src/ejercicios_repaso_h_p_e_04/Loro.java
@@ -13,21 +13,38 @@ public class Loro extends Ave {
 
     String origen;
     Boolean habla;
-    
+
+    public Loro(String nombre, int edad, String estado, String fechaNac, 
+               String pico, Boolean vuela, String origen, Boolean habla) {
+        super(nombre, edad, estado, fechaNac, pico, vuela);
+        this.origen = origen;
+        this.habla = habla;
+    }
     
     @Override
     public void habla() {
-        super.habla();
+        System.out.println("Hola Holita vecinito");
     }
 
     @Override
     public void mostrar() {
-        super.mostrar();
+        System.out.println(toString());
     }
 
     @Override
     public void volar() {
-        
+        if (vuela == true) {
+            System.out.println("Puede volar");
+        }
+        else {
+            System.out.println("No puede volar");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Loro: " + super.toString() + " origen: "
+                + origen + " habla: " + habla;
     }
     
     
