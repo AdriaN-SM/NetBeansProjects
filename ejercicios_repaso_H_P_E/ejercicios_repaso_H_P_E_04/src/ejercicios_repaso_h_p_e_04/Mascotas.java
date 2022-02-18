56beee096608e31ad1109a8c0474c97e9310f0fb
AdriaN-SM <adrianarandiga30@gmail.com>
2022-02-18 18:22:56 +0100

    Añadidos los contructores y algunos comentarios en el código

--- ejercicios_repaso_H_P_E/ejercicios_repaso_H_P_E_04/src/ejercicios_repaso_h_p_e_04/Mascotas.java
+++ ejercicios_repaso_H_P_E/ejercicios_repaso_H_P_E_04/src/ejercicios_repaso_h_p_e_04/Mascotas.java
@@ -10,16 +10,25 @@ package ejercicios_repaso_h_p_e_04;
  * @author adria
  */
 public abstract class Mascotas {
-    
+    //Atributos
     String nombre;
     int edad;
     String estado;
     String fechaNac;
-    
-    
+    //Constructor vacío
+    public Mascotas() {
+    }
+    //Constructor con todos los atributos
+    public Mascotas(String nombre, int edad, String estado, String fechaNac) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.estado = estado;
+        this.fechaNac = fechaNac;
+    }
+    //Métodos abstractos
     public abstract void mostrar();
     public abstract void habla();
-    
+    //Métodos
     public void cumpleaños() {
         
     }
