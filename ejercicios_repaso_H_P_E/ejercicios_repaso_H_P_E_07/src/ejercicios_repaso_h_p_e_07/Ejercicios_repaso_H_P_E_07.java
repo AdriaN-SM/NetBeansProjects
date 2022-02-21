b33fb9bca079a268803c7d8135b6b28a5954217b
AdriaN-SM <adrianarandiga30@gmail.com>
2022-02-21 17:00:31 +0100

    Añadidas las clases, interfaces y métodos de la interfaz Prestable

---
+++ ejercicios_repaso_H_P_E/ejercicios_repaso_H_P_E_07/src/ejercicios_repaso_h_p_e_07/Ejercicios_repaso_H_P_E_07.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ejercicios_repaso_h_p_e_07;
+
+/**
+ *
+ *
+ *
+ * @since 21.02.2022
+ * Fecha de modificación: 21.02.2022
+ * @author Adrian Solanas
+ */
+public class Ejercicios_repaso_H_P_E_07 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        
+    }
+
+}
---
+++ ejercicios_repaso_H_P_E/ejercicios_repaso_H_P_E_07/src/ejercicios_repaso_h_p_e_07/Prestable.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicios_repaso_h_p_e_07;
+
+/**
+ *
+ * @author adria
+ */
+public interface Prestable {
+    public void prestar();
+    public void devolver();
+    public void prestado();
+}
