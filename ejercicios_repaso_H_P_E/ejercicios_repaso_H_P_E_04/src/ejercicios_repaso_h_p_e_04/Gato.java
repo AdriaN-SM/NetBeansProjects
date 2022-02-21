9a8cd71b7f6588079819d1b18233cca26ca9e712
AdriaN-SM <adrianarandiga30@gmail.com>
2022-02-21 20:53:04 +0100

    Completadas las clases Gato y Canario

--- ejercicios_repaso_H_P_E/ejercicios_repaso_H_P_E_04/src/ejercicios_repaso_h_p_e_04/Gato.java
+++ ejercicios_repaso_H_P_E/ejercicios_repaso_H_P_E_04/src/ejercicios_repaso_h_p_e_04/Gato.java
@@ -13,19 +13,40 @@ public class Gato extends Mascotas {
 
     String color;
     Boolean peloLargo;
+
     
+    //Constructor
+    public Gato(String nombre, int edad, String estado, String fechaNac, 
+                String color, Boolean peloLargo) {
+        super(nombre, edad, estado, fechaNac);
+        this.color = color;
+        this.peloLargo = peloLargo;
+    }
     
     @Override
     public void mostrar() {
-        
+        System.out.println(toString());
     }
 
     @Override
     public void habla() {
-        
+        System.out.println("MIAU");
     }
     
-    public void tienePeloLargo() {
-        
+    public String tienePeloLargo() {
+        if (peloLargo) {
+            return "tiene pelo largo.";
+        }
+        else {
+            return "no tiene pelo largo.";
+        }
     }
+
+    @Override
+    public String toString() {
+        return "Gato: " + super.toString() + " color: " + color + " pelo largo: " + tienePeloLargo();
+    }
+    
+    
+    
 }
