a1d8f7e06d4a2bd590613153753207b205eea00e
AdriaN-SM <adrianarandiga30@gmail.com>
2022-04-06 20:16:02 +0200

    Añadido el método para comprobar el índice introducido para la lista

--- ejercicios_colecciones/ejercicios_colecciones_03/src/ejercicios_colecciones_03/Inventario.java
+++ ejercicios_colecciones/ejercicios_colecciones_03/src/ejercicios_colecciones_03/Inventario.java
@@ -13,9 +13,9 @@ import java.util.Iterator;
  * @author adria
  */
 public class Inventario {
-    
+    //Array para almacenar las mascotas
     private ArrayList<Mascotas> mascotas = new ArrayList<>();
-    
+    //método para saber el tipo de mascota
     private String getTipoMascota(Mascotas m) {
         if (m instanceof Perro)
             return "Perro";
@@ -27,12 +27,12 @@ public class Inventario {
             return "Canario";
         return null;
     }
-    
+    //método para vaciar la lista
     protected void vaciarInventario() {
         mascotas.clear();
         System.out.println("Inventario vacío.");
     }
-    
+    //muestra el tipo de mascota y el nombre
     protected void mostrarListaAnimales() {
         int i=0;
         while(i<mascotas.size()){
@@ -40,7 +40,7 @@ public class Inventario {
             i++;
         }
     }
-    
+    //igual que el método anterior pero usando el Iterator
     protected void mostrarListaAnimales2() {
         int i=0;
         Iterator<Mascotas> it = mascotas.iterator();
@@ -50,11 +50,13 @@ public class Inventario {
             i++;
         }
     }
-    
+    //método para mostrar los datos de un animal en concreto
     protected void mostrarUnAnimal(int indice) {
-        System.out.println(mascotas.get(indice));
+        if (comprobarIndice(indice))
+            System.out.println(mascotas.get(indice));
+        System.out.println("El número indicado no está en la lista.");
     }
-    
+    //método para mostrar todos los animales de la lista mediante un Iterator
     protected void mostrarTodosAnimales() {
         int i=0;
         Iterator<Mascotas> it = mascotas.iterator();
@@ -63,7 +65,7 @@ public class Inventario {
             i++;
         }
     }
-    
+    //igual que el método anterior pero sin usar el Iterator
     protected void mostrarTodosAnimales2() {
         int i=0;
         while(i<mascotas.size()){
@@ -71,17 +73,24 @@ public class Inventario {
             i++;
         }
     }
-    
+    //método para insertar un animal a la lista
     protected void insertarAnimal(Mascotas m) {
         mascotas.add(m);
         System.out.println("Animal insertado.");
     }
-    
+    //método para eliminar un animal de la lista
     protected boolean eliminarAnimal(int indice) {
-        if (indice >= 0 && indice < mascotas.size()) {
+        if (comprobarIndice(indice)) {
             mascotas.remove(indice);
             return true;
         }
+        System.out.println("El número indicado no está en la lista.");
+        return false;
+    }
+    //método para comprobar si el número de la lista introducido es correcto
+    private boolean comprobarIndice(int indice) {
+        if (indice > 0 && indice < mascotas.size())
+            return true;
         return false;
     }
 }
