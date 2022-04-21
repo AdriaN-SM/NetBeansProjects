5008b70f22e70c0f88685bd967f0ef171759eab7
AdriaN-SM <adrianarandiga30@gmail.com>
2022-04-21 17:58:38 +0200

    Empezado ejercicio 1 ficheros

---
+++ ejercicios_ficheros/ejercicios_ficheros_01_Leer_de_un_fichero/src/ejercicios_ficheros_01_leer_de_un_fichero/Ejercicios_ficheros_01_Leer_de_un_fichero.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ejercicios_ficheros_01_leer_de_un_fichero;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ *
+ *
+ *
+ * @since 21.04.2022
+ * Fecha de modificación: 21.04.2022
+ * @author Adrian Solanas
+ */
+public class Ejercicios_ficheros_01_Leer_de_un_fichero {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        // TODO code application logic here
+        
+        File archivo = null;
+        FileReader fr = null;
+        BufferedReader br = null;
+
+        try {
+           // Apertura del fichero y creacion de BufferedReader para poder
+           // hacer una lectura comoda (disponer del metodo readLine()).
+
+            archivo = new File (".\\Ejercicio1.txt");
+            fr = new FileReader (archivo);
+            //br = new BufferedReader(fr);
+            int caracter;
+            if(archivo.exists()){
+                String linea;
+                while((caracter=fr.read())!=-1)
+                System.out.print((char)caracter); 
+            }else{
+                System.out.println("EL FICHERO NO EXISTE ANIMAL");
+            }
+            // Lectura del fichero
+
+        }
+        catch(FileNotFoundException filenot){
+            System.err.println("El fichero no existe");
+
+        }finally{
+            // En el finally cerramos el fichero, para asegurarnos
+            // que se cierra tanto si todo va bien como si salta 
+            // una excepcion.
+            try{                    
+                if( null != fr ){   
+                   fr.close();     
+                }                  
+            }catch (Exception e2){ 
+                e2.printStackTrace();
+            }
+        }
+    }
+}
