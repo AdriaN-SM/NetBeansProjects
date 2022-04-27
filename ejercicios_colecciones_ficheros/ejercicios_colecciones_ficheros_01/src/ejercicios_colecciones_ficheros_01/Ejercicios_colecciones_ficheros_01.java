1bc99b227110bb7cd47ee60675d80ed4b52e02ca
AdriaN-SM <adrianarandiga30@gmail.com>
2022-04-27 15:26:21 +0200

    Empezado ejercicios 1 de colecciones y ficheros

---
+++ ejercicios_colecciones_ficheros/ejercicios_colecciones_ficheros_01/src/ejercicios_colecciones_ficheros_01/Ejercicios_colecciones_ficheros_01.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ejercicios_colecciones_ficheros_01;
+
+/**
+ *
+ *
+ *
+ * @since 27.04.2022
+ * Fecha de modificación: 27.04.2022
+ * @author Adrian Solanas
+ */
+public class Ejercicios_colecciones_ficheros_01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        
+    }
+
+}
